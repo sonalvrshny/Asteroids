5b42e5e3415259331da38ac3dcd54ee1ae891ab2
Sonal Varshney <sonalvrshny@gmail.com>
2022-06-12 12:30:26 -0400

    destroy asteroids with bullets

--- asteroids_game/game.py
+++ asteroids_game/game.py
@@ -26,10 +26,11 @@ class Asteroids:
         self.screen = pygame.display.set_mode((1600, 1200))
         # set background image
         self.background = load_image("bg_image.jpg", False)
-        self.spaceship = Spaceship((800,600))
+        self.bullets = []
+        self.asteroids = []
+        self.spaceship = Spaceship((800,600), self.bullets.append)
         # include 6 asteroids at random positions
         # have to make sure that any asteroid does not start in region of spaceship
-        self.asteroids = []
         for _ in range(6):
             while True:
                 position = random_position(self.screen)
@@ -40,7 +41,7 @@ class Asteroids:
 
     # helper method that can be used to return all objects being used in game
     def get_game_objects(self):
-        game_objects = [*self.asteroids]
+        game_objects = [*self.asteroids, *self.bullets]
 
         # have to make sure destroyed spaceship is not added
         if self.spaceship:
@@ -63,6 +64,9 @@ class Asteroids:
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 quit()
+            elif (self.spaceship and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE):
+                self.spaceship.shoot()
+
 
         # if spaceship is not destroyed
         if self.spaceship:
@@ -88,6 +92,19 @@ class Asteroids:
                 if asteroid.collides_with(self.spaceship):
                     self.spaceship = None
                     break
+        
+        # collide with asteroids
+        for bullet in self.bullets[:]:
+            for asteroid in self.asteroids[:]:
+                if asteroid.collides_with(bullet):
+                    self.asteroids.remove(asteroid)
+                    self.bullets.remove(bullet)
+                    break
+
+        # remove bullets if they leave the screen to improve performance
+        for bullet in self.bullets[:]:
+            if not self.screen.get_rect().collidepoint(bullet.position):
+                self.bullets.remove(bullet)
 
 
     def draw(self):
